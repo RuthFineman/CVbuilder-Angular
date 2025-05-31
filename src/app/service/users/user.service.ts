83544a3767cd0df6354f88a2513ae15a59264c23
RuthFineman <ruti8588@gmail.com>
2025-06-01 01:26:00 +0300

    change style

--- src/app/service/users/user.service.ts
+++ src/app/service/users/user.service.ts
@@ -3,26 +3,25 @@ import { environment } from '../../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from '../../models/user';
 import { Observable } from 'rxjs';
-
+import { Inject, PLATFORM_ID } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private apiUrl = `${environment.apiUrl}/Users`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object) {}
 
   getAllUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.apiUrl);
   }
 
   toggleBlockUser(userId: number, newStatus: boolean): Observable<any> {
-
+    
     const token = localStorage.getItem('adminToken');
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${token}`
     });
-  
     return this.http.put(`${this.apiUrl}/${userId}/block/${newStatus}`, null, { headers });
   }
 }
